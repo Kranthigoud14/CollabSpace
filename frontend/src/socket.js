dfbf5266fbc2f02687d7c698c3e947096f66b362
Kranthigoud14 <kranthigoudbalagoni14@gmail.com>
2026-06-14 17:40:13 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,24 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL ||
+  "https://collabspace-iuji.onrender.com";
+
+const socket = io(SOCKET_URL, {
   withCredentials: true,
+  transports: ["websocket", "polling"],
+});
+
+socket.on("connect", () => {
+  console.log("Socket connected:", socket.id);
+});
+
+socket.on("disconnect", (reason) => {
+  console.log("Socket disconnected:", reason);
+});
+
+socket.on("connect_error", (err) => {
+  console.log("Socket error:", err.message);
 });
 
 export default socket;
